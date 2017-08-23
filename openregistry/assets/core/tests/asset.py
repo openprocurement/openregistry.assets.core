d65b802e65c29240c5340188ad232ed7e98be4d7
sasha-kantoriz <sasha-kantoriz@quintagroup.org>
2017-08-23 12:24:31 +0300

    Use CoreResourceTestMixin from API

--- openregistry/assets/core/tests/asset.py
+++ openregistry/assets/core/tests/asset.py
@@ -2,71 +2,11 @@
 import os
 import unittest
 from openregistry.assets.core.tests.base import BaseAssetWebTest
+from openregistry.api.tests.blanks.mixins import CoreResourceTestMixin
 
 
-class AssetResourceTest(BaseAssetWebTest):
+class AssetResourceTest(BaseAssetWebTest, CoreResourceTestMixin):
     relative_to = os.path.dirname(__file__)
-
-    def test_empty_listing(self):
-        response = self.app.get('/')
-        self.assertEqual(response.status, '200 OK')
-        self.assertEqual(response.content_type, 'application/json')
-        self.assertEqual(response.json['data'], [])
-        self.assertNotIn('{\n    "', response.body)
-        self.assertNotIn('callback({', response.body)
-        self.assertEqual(response.json['next_page']['offset'], '')
-        self.assertNotIn('prev_page', response.json)
-
-        response = self.app.get('/?opt_jsonp=callback')
-        self.assertEqual(response.status, '200 OK')
-        self.assertEqual(response.content_type, 'application/javascript')
-        self.assertNotIn('{\n    "', response.body)
-        self.assertIn('callback({', response.body)
-
-        response = self.app.get('/?opt_pretty=1')
-        self.assertEqual(response.status, '200 OK')
-        self.assertEqual(response.content_type, 'application/json')
-        self.assertIn('{\n    "', response.body)
-        self.assertNotIn('callback({', response.body)
-
-        response = self.app.get('/?opt_jsonp=callback&opt_pretty=1')
-        self.assertEqual(response.status, '200 OK')
-        self.assertEqual(response.content_type, 'application/javascript')
-        self.assertIn('{\n    "', response.body)
-        self.assertIn('callback({', response.body)
-
-        response = self.app.get('/?offset=2015-01-01T00:00:00+02:00&descending=1&limit=10')
-        self.assertEqual(response.status, '200 OK')
-        self.assertEqual(response.content_type, 'application/json')
-        self.assertEqual(response.json['data'], [])
-        self.assertIn('descending=1', response.json['next_page']['uri'])
-        self.assertIn('limit=10', response.json['next_page']['uri'])
-        self.assertNotIn('descending=1', response.json['prev_page']['uri'])
-        self.assertIn('limit=10', response.json['prev_page']['uri'])
-
-        response = self.app.get('/?feed=changes')
-        self.assertEqual(response.status, '200 OK')
-        self.assertEqual(response.content_type, 'application/json')
-        self.assertEqual(response.json['data'], [])
-        self.assertEqual(response.json['next_page']['offset'], '')
-        self.assertNotIn('prev_page', response.json)
-
-        response = self.app.get('/?feed=changes&offset=0', status=404)
-        self.assertEqual(response.status, '404 Not Found')
-        self.assertEqual(response.content_type, 'application/json')
-        self.assertEqual(response.json['status'], 'error')
-        self.assertEqual(response.json['errors'], [
-            {u'description': u'Offset expired/invalid', u'location': u'querystring', u'name': u'offset'}
-        ])
-
-        response = self.app.get('/?feed=changes&descending=1&limit=10')
-        self.assertEqual(response.status, '200 OK')
-        self.assertEqual(response.content_type, 'application/json')
-        self.assertEqual(response.json['data'], [])
-        self.assertIn('descending=1', response.json['next_page']['uri'])
-        self.assertIn('limit=10', response.json['next_page']['uri'])
-        self.assertNotIn('descending=1', response.json['prev_page']['uri'])
-        self.assertIn('limit=10', response.json['prev_page']['uri'])
 
 
 def suite():
