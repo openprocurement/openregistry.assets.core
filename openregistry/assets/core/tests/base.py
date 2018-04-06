6061fd61b44a629dd9d9bf298aad0065b6c125e5
Scandie <grizly.vl@gmail.com>
2018-04-06 14:36:26 +0300

    Add forwarding imports for assets packages

--- openregistry/assets/core/tests/base.py
+++ openregistry/assets/core/tests/base.py
@@ -1,7 +1,18 @@
 # -*- coding: utf-8 -*-
 from datetime import datetime
 
-from openprocurement.api.tests.base import BaseResourceWebTest
+from openprocurement.api.tests.base import (
+    BaseResourceWebTest,
+    DumpsTestAppwebtest,  # noqa forwarded import
+    PrefixedRequestClass  # noqa forwarded import
+)
+from openprocurement.api.tests.blanks.json_data import (
+    test_document_data,  # noqa forwarded import
+    test_asset_basic_data,  # noqa forwarded import
+    test_asset_basic_data_with_schema,  # noqa forwarded import
+    test_asset_claimrights_data,  # noqa forwarded import
+    test_asset_compound_data  # noqa forwarded import
+)
 
 
 now = datetime.now()
--- openregistry/assets/core/tests/blanks/mixins.py
+++ openregistry/assets/core/tests/blanks/mixins.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
 
 from openprocurement.api.tests.base import snitch
+from openprocurement.api.tests.blanks.mixins import (
+    ResourceTestMixin,  # noqa forwarded import
+    ResourceDocumentTestMixin  # noqa forwarded import
+)
 
 
 from .asset import (
