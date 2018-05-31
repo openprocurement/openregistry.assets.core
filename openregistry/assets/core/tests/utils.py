b1b154c56fe952d240c185e590dbf74dd71b506f
MichaelYusko <mykhaylo.yusko@gmail.com>
2018-05-31 17:25:22 +0300

    Add mocked configurator_project object

--- openregistry/assets/core/tests/utils.py
+++ openregistry/assets/core/tests/utils.py
@@ -1,3 +1,5 @@
+import mock
+
 from openregistry.assets.core.utils import generate_asset_id
 from openregistry.assets.core.tests.base import BaseAssetWebTest
 from openprocurement.api.utils import get_now
@@ -6,14 +8,20 @@ from openprocurement.api.utils import get_now
 class AssetUtilTest(BaseAssetWebTest):
     def setUp(self):
         self.database = self.db
-        self.base_asset_prefix = 'UA-AR-DGF'
 
     def test_generate_id(self):
+        mock_project_configurator = mock.patch(
+            'openregistry.assets.core.utils.project_configurator',
+            autospec=True
+        )
+        asset_prefix = 'UA-AR-DGF'
+        mock_project_configurator.ASSETS_PREFIX = asset_prefix
+
         result = generate_asset_id(get_now(), self.db)
         key = get_now().date().isoformat()
         index = self.db.get(key, 1)
         mock_id = '{}-{:04}-{:02}-{:02}-{:06}{}'.format(
-            self.base_asset_prefix,
+            asset_prefix,
             get_now().year,
             get_now().month,
             get_now().day,
