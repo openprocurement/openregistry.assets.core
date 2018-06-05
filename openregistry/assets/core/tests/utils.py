0d1dcebfb0b198132f05d70cbcd7dee024be5f79
MichaelYusko <mykhaylo.yusko@gmail.com>
2018-06-05 15:03:01 +0300

    Little refactoring

--- openregistry/assets/core/tests/utils.py
+++ openregistry/assets/core/tests/utils.py
@@ -5,19 +5,17 @@ from openregistry.assets.core.tests.base import BaseAssetWebTest
 from openprocurement.api.utils import get_now
 
 
+@mock.patch('openregistry.assets.core.utils.project_configurator', autospec=True)
 class AssetUtilTest(BaseAssetWebTest):
+
     def setUp(self):
         self.database = self.db
 
-    def test_generate_id(self):
-        mock_project_configurator = mock.patch(
-            'openregistry.assets.core.utils.project_configurator',
-            autospec=True
-        )
-        test_prefix = 'UA-AR-DGF'
-        mock_project_configurator.ASSETS_PREFIX = test_prefix
-
+    def test_generate_id(self, mock_project_configurator):
+        test_prefix = 'TEST-ID'
+        mock_project_configurator.ASSET_PREFIX = test_prefix
         result = generate_asset_id(get_now(), self.db)
+
         key = get_now().date().isoformat()
         index = self.db.get(key, 1)
         mock_id = '{}-{:04}-{:02}-{:02}-{:06}{}'.format(
