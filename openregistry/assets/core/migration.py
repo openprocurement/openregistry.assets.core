5db781d7c1e8baabac7e06675a4769b2d73f695e
Bohdan Dmytriv <dmytriv@quintagroup.org>
2018-10-08 15:42:11 +0300

    Import forward

--- openregistry/assets/core/migration.py
+++ openregistry/assets/core/migration.py
@@ -1,5 +1,10 @@
 # -*- coding: utf-8 -*-
 import logging
+
+from openprocurement.api.migration import (
+    BaseMigrationsRunner,  # noqa: forwarded import
+    BaseMigrationStep,  # noqa: forwarded import
+)
 LOGGER = logging.getLogger(__name__)
 
 
